52197fe895297d98833617421c688f469ea31708
Cristi Duma <cristi.duma@aissac.ro>
2009-04-07 15:59:05 +0300

    more specific when choosing image size, with a contextual popup

--- tiny_paper_extension.rb
+++ tiny_paper_extension.rb
@@ -9,6 +9,7 @@ class TinyPaperExtension < Radiant::Extension
   define_routes do |map|
     map.with_options(:controller => 'admin/tiny_paper') do |asset|
       asset.images            "/admin/tiny_paper/images",                 :action => 'images'
+      asset.images_sizes      "/admin/tiny_paper/:id/images_sizes",       :action => 'images_sizes'
       asset.files             "/admin/tiny_paper/files",                  :action => 'files'
       asset.pages             "/admin/tiny_paper/pages",                  :action => 'pages'
       asset.create            "/admin/tiny_paper/create",                 :action => 'create'
